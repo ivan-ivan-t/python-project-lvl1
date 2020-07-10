687708434ea72be2200ac96da8f02ddc0d6f55e4
Ivan Terehov <ivterehov@gmail.com>
2020-07-10 19:58:29 +0300

    corrections code

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,15 +4,8 @@ import random
 RULES = 'Answer "yes" if number even otherwise answer "no"\n'
 
 
-def is_even(number):
-    if number % 2 == 0:
-        return 'yes'
-    else:
-        return 'no'
-
-
 def question_and_correct_answer():
     number = random.randint(1, 50)
     question = str(number)
-    correct_answer = is_even(number)
+    correct_answer = 'yes' if number % 2 == 0 else 'no'
     return (correct_answer, question)
