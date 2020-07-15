d1d4f3ac8c31cb1e894f20b8247c723d3ddfb49d
Ivan Terehov <ivterehov@gmail.com>
2020-07-15 14:40:13 +0300

    refactoring brain-prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,16 +4,18 @@ import random
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no"'
 
 
-def is_prime(number):
-    for i in range(2, number):
-        if number % i == 0:
-            return 'no'
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, round(num/2)):
+        if num % i == 0:
+            return False
     else:
-        return 'yes'
+        return True
 
 
 def prepare_question_and_calculated():
-    number = random.randint(2, 100)
-    question = str(number)
-    correct_answer = is_prime(number)
+    num = random.randint(1, 100)
+    question = str(num)
+    correct_answer = 'yes' if is_prime(num) else 'no'
     return (correct_answer, question)
